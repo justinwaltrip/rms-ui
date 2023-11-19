fe3dbb72436551d0e2fdb080dc573ec050a8b145
Justin Waltrip <justin.waltrip@protonmail.com>
2023-11-18 23:37:40 -0500

    Add menu and close tab functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{Menu, MenuItem, Submenu};
+
 fn main() {
+    let submenu = Submenu::new(
+        "rms",
+        Menu::new()
+            .add_native_item(MenuItem::Minimize)
+            .add_native_item(MenuItem::Quit),
+    );
+    let menu = Menu::new().add_submenu(submenu);
     tauri::Builder::default()
+        .menu(menu)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/title-bar/TitleBar.tsx
+++ src/components/title-bar/TitleBar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import "./TitleBar.css";
 import { FC } from "react";
 
@@ -20,43 +21,62 @@ function createFile(
   setActiveFileIndex(openFiles.length);
 }
 
+function closeTab(
+  index: number,
+  openFiles: Array<string>,
+  setOpenFiles: (openFiles: Array<string>) => void,
+) {
+  if (openFiles.length === 1) {
+    setOpenFiles([]);
+  } else {
+    setOpenFiles(openFiles.filter((_, i) => i !== index));
+  }
+}
+
 const TitleBar: FC<TitleBarProps> = ({
   openFiles,
   setOpenFiles,
   activeFileIndex,
   setActiveFileIndex,
-}) => (
-  <div data-tauri-drag-region className="title-bar">
-    <img
-      className="create-icon"
-      src={create}
-      alt="Create icon"
-      onClick={() => createFile(openFiles, setOpenFiles, setActiveFileIndex)}
-    />
-    {openFiles.map((file, index) => (
-      <div
-        key={index}
-        className={`tab ${
-          index === activeFileIndex ? "active-tab" : "inactive-tab"
-        }`}
-        onClick={() => setActiveFileIndex(index)}
-      >
-        <p className="tab-label">{file ? file : "New tab"}</p>
-        <img
-          className="close-icon"
-          src={close}
-          alt="Close icon"
-          onClick={() => {
-            if (openFiles.length === 1) {
-              setOpenFiles([]);
-            } else {
-              setOpenFiles(openFiles.filter((_, i) => i !== index));
-            }
-          }}
-        />
-      </div>
-    ))}
-  </div>
-);
+}) => {
+  document.addEventListener("keydown", (e) => {
+    console.log(e);
+    // on command + w, close the active tab
+    if (e.metaKey && e.key === "w") {
+      closeTab(
+        activeFileIndex,
+        openFiles,
+        setOpenFiles,
+      );
+    }
+  });
+  return (
+    <div data-tauri-drag-region className="title-bar">
+      <img
+        className="create-icon"
+        src={create}
+        alt="Create icon"
+        onClick={() => createFile(openFiles, setOpenFiles, setActiveFileIndex)}
+      />
+      {openFiles.map((file, index) => (
+        <div
+          key={index}
+          className={`tab ${
+            index === activeFileIndex ? "active-tab" : "inactive-tab"
+          }`}
+          onClick={() => setActiveFileIndex(index)}
+        >
+          <p className="tab-label">{file ? file : "New tab"}</p>
+          <img
+            className="close-icon"
+            src={close}
+            alt="Close icon"
+            onClick={() => closeTab(index, openFiles, setOpenFiles)}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default TitleBar;
