968f3e6f9705e5b8405c7ac2a57eaaeb85626751
Justin Waltrip <justin.waltrip@protonmail.com>
2023-12-31 10:38:54 -0500

    Add e2e tests and update tsconfig.json

---
+++ e2e/home.test.ts
@@ -0,0 +1,25 @@
+/**
+ * As a user, I want to create a new recipe collection.
+ *
+ * Steps:
+ * 1. Go to the home page
+ * 2. Click on the "Create" button
+ * 3. Enter a name for the collection
+ * 4. Choose a location for the collection
+ * 5. Click on the "Create" button
+ *
+ * Expected:
+ * - A new folder is created in the chosen location with the given name
+ * - The collection is displayed in the home page sidebar
+ *
+ *
+ * As a user, I want to open an existing recipe collection.
+ *
+ * Steps:
+ * 1. Go to the home page
+ * 2. Click on the "Open" button
+ * 3. Choose the folder of the collection
+ *
+ * Expected:
+ * - The collection is displayed in the home page sidebar
+ */
