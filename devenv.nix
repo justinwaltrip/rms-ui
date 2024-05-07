d5546eff3bff1685f8e2321f911590d2020ef23a
Justin Waltrip <justin.waltrip@protonmail.com>
2024-05-06 23:56:07 -0400

    Update devenv configuration to include additional frameworks and environment variables

--- devenv.nix
+++ devenv.nix
@@ -1,5 +1,6 @@
-{ pkgs, lib, config, inputs, ... }:
+{ pkgs, lib, config, inputs, env, ... }:
 
+let frameworks = pkgs.darwin.apple_sdk.frameworks; in
 {
   languages.javascript = {
     enable = true;
@@ -9,4 +10,26 @@
     };
   };
   languages.rust.enable = true;
+  # https://discourse.nixos.org/t/compile-bevy-with-nix-developer-environment-on-macos/31512/3
+  packages = with pkgs; [
+    gcc
+    darwin.libobjc
+    darwin.libiconv
+    frameworks.Security
+    frameworks.CoreServices
+    frameworks.CoreFoundation
+    frameworks.AppKit
+    frameworks.Foundation
+    frameworks.ApplicationServices
+    frameworks.CoreGraphics
+    frameworks.CoreVideo
+    frameworks.Carbon
+    frameworks.IOKit
+    frameworks.CoreAudio
+    frameworks.AudioUnit
+    frameworks.QuartzCore
+    frameworks.Metal
+    frameworks.WebKit
+  ];
+  env.CFLAGS = lib.mkForce ("-I${pkgs.darwin.libobjc}/include/");
 }
