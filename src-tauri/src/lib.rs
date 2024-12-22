488b68ca53db52c1be0dbf6d0ef2b6106aa6c2d0
Justin Waltrip <justin.waltrip@protonmail.com>
2024-12-21 21:53:57 -0500

    Add iCloud plugin support: Integrate tauri-plugin-icloud and update schemas for permissions

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -63,6 +63,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_icloud::init())
         // .setup(|app| {
         //     let app_menu = SubmenuBuilder::new(app, "rms")
         //         .minimize()
